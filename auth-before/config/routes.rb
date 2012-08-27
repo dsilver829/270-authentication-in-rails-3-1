213aef763695eced9c7b0014f19eb1ae6ddebb4f
David Silver <dsilver@candidatemetrics.com>
2012-08-26 20:58:49 -0400

    set up

--- auth-before/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Auth::Application.routes.draw do
-  get "logout" => "sessions#destroy", :as => "logout"
-  get "login" => "sessions#new", :as => "login"
-  get "signup" => "users#new", :as => "signup"
-  root :to => "users#new"
-  resources :users
-  resources :sessions
-  get "secret" => "secret#index"
-end
--- auth-before/test/unit/helpers/secret_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SecretHelperTest < ActionView::TestCase
-end
